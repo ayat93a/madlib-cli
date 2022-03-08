f51a05df0728a75aebcbf87f3c11dee9f6925316
Ayat <alkayedayat93@gmail.com>
2022-03-08 14:46:46 +0300

    resubmit1

--- madlib-cli/madlib_cli/madlib.py
+++ madlib-cli/madlib_cli/madlib.py
@@ -1,12 +1,12 @@
 
 from cgitb import text
 import re
-# print(''' Hello player \U0001f600 ! Welcome to my Mad Lib Game , The World's Greatest Word Game \U0001F970
-# my game is based on create a crazy story for you \U0001F923 based in your words 
-# i will ask you to give me some words and i will tell you a magic story \U0001F602
-# please enter your word \U0001f600 the words that you will type should be in a specific type , i.e noun , adjective , ... etc.
-# HAVE FUN \U0001F609 !
-#  ''')
+print(''' Hello player \U0001f600 ! Welcome to my Mad Lib Game , The World's Greatest Word Game \U0001F970
+my game is based on create a crazy story for you \U0001F923 based in your words 
+i will ask you to give me some words and i will tell you a magic story \U0001F602
+please enter your word \U0001f600 the words that you will type should be in a specific type , i.e noun , adjective , ... etc.
+HAVE FUN \U0001F609 !
+ ''')
 
 # file_path = '../assets/dark_and_stormy_night_template.txt'
 def read_template(file_path):
@@ -43,7 +43,7 @@ def merge(parts ,user_input):
 
 
 if __name__=='__main__': 
-  text= read_template('../assets/dark_and_stormy_night_template.txt')
+  text= read_template('madlib.txt')
   parts , stripped = parse_template(text)
   user_input= userinput(stripped )
   output = merge (parts,user_input)
