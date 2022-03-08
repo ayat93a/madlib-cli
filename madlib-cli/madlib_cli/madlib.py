acb22683723fe32d37cdc71fdf0d5d3c826794d6
Ayat <alkayedayat93@gmail.com>
2022-03-08 11:56:06 +0300

    reordered

--- madlib-cli/madlib_cli/madlib.py
+++ madlib-cli/madlib_cli/madlib.py
@@ -1,12 +1,12 @@
 
 from cgitb import text
 import re
-print(''' Hello player \U0001f600 ! Welcome to my Mad Lib Game , The World's Greatest Word Game \U0001F970
-my game is based on create a crazy story for you \U0001F923 based in your words 
-i will ask you to give me some words and i will tell you a magic story \U0001F602
-please enter your word \U0001f600 the words that you will type should be in a specific type , i.e noun , adjective , ... etc.
-HAVE FUN \U0001F609 !
- ''')
+# print(''' Hello player \U0001f600 ! Welcome to my Mad Lib Game , The World's Greatest Word Game \U0001F970
+# my game is based on create a crazy story for you \U0001F923 based in your words 
+# i will ask you to give me some words and i will tell you a magic story \U0001F602
+# please enter your word \U0001f600 the words that you will type should be in a specific type , i.e noun , adjective , ... etc.
+# HAVE FUN \U0001F609 !
+#  ''')
 
 # file_path = '../assets/dark_and_stormy_night_template.txt'
 def read_template(file_path):
@@ -27,23 +27,26 @@ def parse_template(text):
     parts= tuple(re.findall(r'{(.*?)}', text))
     return  stripped , parts
 
-def userinput(parts):
+def userinput(stripped ):
     user_input=[]
-    for i in parts:
+    # parts=list(parts)
+    for i in stripped :
         user_input.append(input(i))
+        # print(i)
     return user_input
 
-def merge(stripped ,user_input):
-    stripped=str(stripped)
-    text= stripped.format(*user_input)
+
+def merge(parts ,user_input):
+    parts=str(parts)
+    text= parts.format(*user_input)
     return text
 
 
 if __name__=='__main__': 
   text= read_template('../assets/dark_and_stormy_night_template.txt')
   parts , stripped = parse_template(text)
-  user_input= userinput(parts)
-  output = merge (user_input,stripped)
+  user_input= userinput(stripped )
+  output = merge (parts,user_input)
   print(output)
 
     
